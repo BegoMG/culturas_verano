510c40192106e86644c650b48cb38c545fb0401a
BegoMG <BegoMG@users.noreply.github.com>
2025-06-20 15:54:12 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,290 @@
+const libros = [
+  {
+    recomendador: "Jordi Juan",
+    fotoRecomendador: "img/avatares/autor1.jpeg",
+    caratula: "img/portadas/libro1.jpeg",
+    titulo: "El nuevo espíritu del mundo",
+    autor: "Esteban Hernández",
+    editorial: "Deusto",
+    resenya: "De Esteban Hernández siempre me ha llamado la atención la facilidad con la que describe los conflictos e incertezas que nos rodean y cómo aporta siempre una mirada lúcida que te invita a pensar. Es lo mejor que le podemos pedir a un ensayista. Lo ha vuelto a hacer con su último libro, El nuevo espíritu del mundo, que está de vigente actualidad por las políticas de Donald Trump. El mundo al que vamos es cada día más incierto, pero Esteban nos proporciona una brújula para orientarnos."
+  },
+  {
+    recomendador: "Miquel Molina",
+    fotoRecomendador: "img/avatares/autor2.jpeg",
+    caratula: "img/portadas/libro2.jpeg",
+    titulo: "Canon de cámara oscura",
+    autor: "Enrique Vila-Matas",
+    editorial: "Seix Barral",
+    resenya: "Si lo que se busca con lectura de verano es la evasión y la desconexión, qué mejor que perderse por el laberinto literario que propone la última novela de Enrique Vila Matas. Es una narración exquisita que nos permite visitar rincones apenas explorados de la literatura."
+  },
+  {
+    recomendador: "Xavi Ayén",
+    fotoRecomendador: "img/avatares/autor3.jpeg",
+    caratula: "img/portadas/libro3.jpeg",
+    titulo: "Imposible decir adiós / Els adeus impossibles",
+    autor: "Han Kang",
+    editorial: "Random House/ La Magrana",
+    resenya: "En verano, resulta refrescante leer novelas en las que haga mucho frío. Y nadie describe mejor la nieve que los dos últimos premios Nobel de Literatura, el noruego Jon Fosse y la coreana Han Kang. En esta historia, una mujer acude a la remota isla de Jeju con la misión de alimentar a la cotorra de una amiga que ha ingresado de urgencias en Seúl tras perder unos dedos en un accidente de carpintería. Llegar a la casa donde peligra la vida del ave será una epopeya que implica atravesar una atroz tormenta de nieve. La novela aborda los ecos (y los fantasmas) de una masacre cometida por el gobierno coreano a finales de los años cuarenta."
+  },
+    {
+    recomendador: "Sílvia Colomé",
+    fotoRecomendador: "img/avatares/autor4.jpeg",
+    caratula: "img/portadas/libro4.jpeg",
+    titulo: "El imperio infinito",
+    autor: "Aldo Cazzullo",
+    editorial: "HarperCollins",
+    resenya: "Podría ser otro libro sobre Roma, desde su origen legendario hasta el final del imperio, pero el periodista italiano consigue rescatar conocimientos y añadir nuevos en una ágil, atractiva e incluso adictiva escritura que nos permite viajar por la historia de la ciudad eterna, recordándonos hasta qué punto esta civilización, de la que somos herederos, pervive en nuestros días."
+  },
+  {
+    recomendador: "Ignasi Moya",
+    fotoRecomendador: "img/avatares/autor.webp",
+    caratula: "img/portadas/libro5.jpeg",
+    titulo: "Nadie nos llamará antepasados",
+    autor: "Bruno Galindo",
+    editorial: "Libros del K.O.",
+    resenya: "A principios del pasado siglo, Guillermo Larregui emigró a Argentina, país que cruzó a pie, de la Patagonia a Iguazú, movido por una apuesta. Casi un siglo después, Bruno Galindo -escritor viajero, periodista, músico, gestor cultural- le sigue la pista al tiempo que indaga en su pasado familiar, en Argentina, en España, Yugoslavia o el Imperio Austrohúngaro. Dos fascinantes historias entrecruzadas sobre la aventura, la vida nómada, la escapada, la emigración… y cómo los secretos y heridas de nuestros antepasados nos construyen."
+  },
+  {
+    recomendador: "Juan Ángel Juristo",
+    fotoRecomendador: "img/avatares/autor.webp",
+    caratula: "img/portadas/libro6.jpeg",
+    titulo: "Cárcel de tinieblas",
+    autor: "Juan Manuel de Prada",
+    editorial: "Espasa",
+    resenya: "Esta segunda parte de la novela de Juan Manuel de Prada, Mil ojos esconde la noche, es el final de uno de los proyectos literarios de más alcance que se ha visto en la literatura en castellano de los últimos años, al margen de modas. El autor describe el París de la Ocupación con una imaginación desbordante, una muy buena documentación y un lenguaje que alcanza a Cervantes y Quevedo en sus referencias."
+  },
+    {
+    recomendador: "Inés Macpherson",
+    fotoRecomendador: "img/avatares/autor.webp",
+    caratula: "img/portadas/libro7.webp",
+    titulo: "Que morin els fills dels altres",
+    autor: "Roser Cabré-Verdiell",
+    editorial: "Males Herbes",
+    resenya: "Una novela potente y fascinante que interpela al lector y le invita a entender la transformación de Rebecca, la protagonista. Una historia que habla del miedo que paraliza y el deseo como motor, de la vida como encrucijada, como viaje donde hay que decidir quién eres, quién podrías ser, cómo definirte. Maternidad, pérdida, muerte y familia bañados por el mar y la arena de Ocata."
+  },
+    {
+    recomendador: "Blai Felip",
+    fotoRecomendador: "img/avatares/autor8.jpeg",
+    caratula: "img/portadas/libro8.webp",
+    titulo: "El rellotge verd",
+    autor: "Julià Guillamon",
+    editorial: "Anagrama",
+    resenya: "Una crónica-río personal de un tiempo en que los lectores de cierta edad se sentirán reflejados y se sorprenderán de los recuerdos que todavía guardan en el cerebro. Escrita con maestría, ironía, distancia, autocrítica y un punto de acidez, recuerda la manera de hacer planiana. Escribir con esta sencillez literaria es muy difícil."
+  },
+    {
+    recomendador: "Albert Lladó",
+    fotoRecomendador: "img/avatares/autor9.jpeg",
+    caratula: "img/portadas/libro9.jpeg",
+    titulo: "Esta cosa de tinieblas / Això tan tenebrós",
+    autor: "Mar García Puig",
+    editorial: "Debate / La Magrana",
+    resenya: "La autora, que hibrida con lucidez diversos géneros, nos recuerda que, cuando hablamos, pronunciamos seis metáforas por minuto. Y, precisamente, las metáforas son una herramienta de libertad ante la literalidad de un mundo que ya no cree en la imaginación del lenguaje. 'Hablamos como vivimos', nos recuerda."
+  },
+    {
+    recomendador: "Fèlix Riera",
+    fotoRecomendador: "img/avatares/autor10.jpeg",
+    caratula: "img/portadas/libro10.jpeg",
+    titulo: "Un duelo interminable: La batalla cultural del largo siglo XX",
+    autor: "José Enrique Ruiz-Domènec",
+    editorial: "Taurus",
+    resenya: "Nos narra una imprescindible crónica de la guerra cultural, que abarca desde 1871 al 2021, para demostrar que uno de los motores de la Historia es y ha sido la guerra cultural desarrollada durante el pasado siglo; una guerra tan intensa en su disputa de conceptos e ideas que ha llegado hasta a apropiarse y servirse de acontecimientos de la antigua Roma, de la Edad Media, del Renacimiento y de la Ilustración para lograr imponerse unas a otras."
+  },
+    {
+    recomendador: "Laura Freixas",
+    fotoRecomendador: "img/avatares/autor11.jpeg",
+    caratula: "img/portadas/libro11.jpeg",
+    titulo: "Una familia moderna",
+    autor: "Helga Flatland",
+    editorial: "Nórdica",
+    resenya: "Una pareja mayor convoca a hijos y nietos a celebrar los setenta años del abuelo. Pero el feliz cumpleaños esconde una sorpresa… Compuesta por los monólogos de los personajes, Una familia moderna nos invita a reflexionar sobre la relación entre cónyuges, padres, hijos, hermanos… Una novela inteligente y sensible, en la que de un modo u otro nos reconocemos todas/os."
+  },
+      {
+    recomendador: "Jordi Llavina",
+    fotoRecomendador: "img/avatares/autor12.jpeg",
+    caratula: "img/portadas/libro12.jpg",
+    titulo: "Els ocells / Los pájaros",
+    autor: "Tarjei Vesaas",
+    editorial: "Club Editor / Nórdica",
+    resenya: "¡La mejor novela que he leído en meses! Protagonizada por un muchacho que parece un paleto, pero que acaso resulte un visionario. Su hermana querría no tener que estar tan pendiente de él. Una historia sobre el miedo a la muerte. Llena de magia, y desplegada por una escritura lírica nada empalagosa."
+  },
+      {
+    recomendador: "Cristina Jolonch",
+    fotoRecomendador: "img/avatares/autor13.jpeg",
+    caratula: "img/portadas/libro13.jpg",
+    titulo: "Un camarero en París",
+    autor: "Edward Chisholm",
+    editorial: "Ático de los Libros",
+    resenya: "¿Hay algo más complejo para un joven inglés que sueña ser escritor en París que empezar como camarero? Edward Chisholm deja claro que sí: sobrevivir como ayudante de camarero. La experiencia en el inframundo parisino y el desprecio a quienes ocupan el eslabón más bajo en los restaurantes, especialmente si no hablan la lengua de Molière, nutre su primera novela."
+  },
+      {
+    recomendador: "José Enrique Ruiz Domènec",
+    fotoRecomendador: "img/avatares/autor14.jpeg",
+    caratula: "img/portadas/libro14.webp",
+    titulo: "Crítica de la razón maquinal. Ensayo de filosofía agonística",
+    autor: "Basilio Baltasar",
+    editorial: "KRK Ediciones",
+    resenya: "Para que el lector afronte la situación actual ante el peso de la tecnología con una reflexión en profundidad cuyo leve centelleo evoca las aguas de una pequeña cala mediterránea; para que el lector haga suyos los aforismos aquí contenidos que iluminan el futuro y cuya pericia procede de los antiguos jeroglíficos de una sabiduría inagotable."
+  },
+      {
+    recomendador: "Joana Bonet",
+    fotoRecomendador: "img/avatares/autor15.jpeg",
+    caratula: "img/portadas/libro15.jpeg",
+    titulo: "El derecho a las cosas bellas",
+    autor: "Juan Evaristo Valls Boix",
+    editorial: "Ariel",
+    resenya: "Más que un libro se trata de un manifiesto, un movimiento que invita a la vida horizontal con libertad y holgura. También una vindicación del hedonismo a través de autores que educaron el gusto por las cosas brillantes y bellas excluyéndolas de la noción de privilegio."
+  },
+      {
+    recomendador: "Joan Esculies",
+    fotoRecomendador: "img/avatares/autor16.jpeg",
+    caratula: "img/portadas/libro16.jpg",
+    titulo: "Contes",
+    autor: "William Faulkner",
+    editorial: "Edicions de 1984",
+    resenya: "Mil páginas ideales para un verano caluroso y largo. Cuarenta y dos cuentos que el propio William Faulkner escogió y que hablan de los grandes temas de su obra –el racismo, la violencia, la fatalidad, la grandeza y la estupidez humana–. Situados en el sur de Estados Unidos, pero también en la Primera Guerra Mundial o Beverly Hills. Para comprender las raíces de los conflictos de la América actual."
+  },
+        {
+    recomendador: "David Castillo",
+    fotoRecomendador: "img/avatares/autor.webp",
+    caratula: "img/portadas/libro17.jpeg",
+    titulo: "Londres",
+    autor: "Louis-Ferdinand Céline",
+    editorial: "Anagrama / Proa",
+    resenya: "La segunda de sus novelas recuperadas tras ser robadas de casa del escritor después de la Segunda Guerra Mundial, cuando fue acusado de colaboracionista. Junto con la primera, Guerra, constituye un testimonio, también autobiográfico, de la vida del novelista, que hizo saltar los cimientos de la literatura francesa y despertó la atención de figuras como Henry Miller y los beatniks. Los meses posteriores a la Gran Guerra, dónde Céline fue herido de gravedad, y la capital inglesa son los cimientos de esta entretenida novela."
+  },
+      {
+    recomendador: "Inés Pich Aguilera",
+    fotoRecomendador: "img/avatares/autor.webp",
+    caratula: "img/portadas/libro18.jpeg",
+    titulo: "Como bestias",
+    autor: "Violaine Bérot",
+    editorial: "Las afueras",
+    resenya: "Desde el atril del menosprecio se expresan los insatisfechos para machacar a cualquiera que sea diferente. Violaine Bérot da forma a este tipo de mezquindad en un relato que hace de espejo al mal que nos devora por dentro. Una bofetada a mano abierta para reflexionar sobre el trato que ofrecemos."
+  },
+        {
+    recomendador: "Carlos Zanón",
+    fotoRecomendador: "img/avatares/autor19.jpeg",
+    caratula: "img/portadas/libro19.jpeg",
+    titulo: "Silencios que matan",
+    autor: "Jordan Harper",
+    editorial: "Salamandra",
+    resenya: "Precedida por un montón de premios, esta novela hace honor a tanto ruido. Es de las mejores de género negro que he leído en años. Trata sobre qué hay detrás del Hollywood de las series televisivas y la máquina de destrozar sueños adolescentes de fama y notoriedad. Es rápida sin ser aturullada, brillante sin ser petulante, amarga e inolvidable. Literatura popular de altos vuelos."
+  },
+      {
+    recomendador: "Olga Merino",
+    fotoRecomendador: "img/avatares/autor20.jpeg",
+    caratula: "img/portadas/libro20.jpeg",
+    titulo: "El español que enamoró al mundo",
+    autor: "Ignacio Peyró",
+    editorial: "Libros del Asteroide",
+    resenya: "Las canciones de Julio Iglesias son para el verano, lo mismo que este retrato del crooner superventas, una semblanza que trasciende el material biográfico para esbozar la crónica sentimental de un país entero, desde el tardofranquismo y las camisas viejas hasta el pelotazo y las mariscadas. El autor despliega una capacidad de observación y un manejo del lenguaje extraordinarios."
+  },
+        {
+    recomendador: "Antonio Lozano",
+    fotoRecomendador: "img/avatares/autor21.jpeg",
+    caratula: "img/portadas/libro21.jpeg",
+    titulo: "El asesinato de los Aosawa",
+    autor: "Riku Onda",
+    editorial: "Salamandra",
+    resenya: "Un día caluroso, resguárdese con una limonada -preparada por usted, ya lo entenderá- y déjese llevar a una mansión en la costa japonesa. Un día igual de tórrido, aunque de 1970, los invitados a una fiesta perecerán, excepto una chica ciega con un recuerdo de tonos azulados. El cómo y el quién fabulado por Riku Onda en El asesinato de los Aosawa le deslumbrarán."
+  },
+      {
+    recomendador: "Magí Camps",
+    fotoRecomendador: "img/avatares/autor22.jpeg",
+    caratula: "img/portadas/libro22.webp",
+    titulo: "L’home de la casa",
+    autor: "Enric Pardo Ramírez",
+    editorial: "La Magrana",
+    resenya: "El guionista Enric Pardo se pone en la piel de un Enric niño para contarnos de un modo cautivador qué le supuso ser el hijo de los primeros padres divorciados de Onda, la población de Castellón donde vivía. Con la mirada limpia e inocente de un niño que no entiende exactamente lo que está sucediendo en su casa, Pardo nos transporta a los años ochenta para descubrir quién es el hombre de la casa."
+  },
+        {
+    recomendador: "Eva Muñoz",
+    fotoRecomendador: "img/avatares/autor.webp",
+    caratula: "img/portadas/libro23.jpeg",
+    titulo: "La habitación de las ahogadas",
+    autor: "Alana S. Portero",
+    editorial: "La Bella Varsovia",
+    resenya: "La mejor Alana S. Portero está en su poesía. Es ahí donde la autora forjó la voz que luego construyó su narrativa; una voz de una gran intensidad y altísima calidad lírica. El poemario La habitación de las ahogadas es la búsqueda de una identidad y parece un conjuro a vida o muerte; enraíza en lo ancestral desde un pulso contemporáneo."
+  },
+      {
+    recomendador: "Toni Montesinos",
+    fotoRecomendador: "img/avatares/autor24.jpeg",
+    caratula: "img/portadas/libro24.webp",
+    titulo: "Bahía de Sal",
+    autor: "Gabriela Guerra",
+    editorial: "Traveler",
+    resenya: "No puede llamarse menos que obra maestra esta historia, realista-mágica y metafórica, lírica y crítica, sobre un pueblo costero y las vicisitudes de unas gentes enfrentadas a la vida más descarnada. Ganadora del premio Juan Rulfo 2016, es la primera de una trilogía de la que ya se ha publicado Avándaro, la segunda, en la misma editorial."
+  },
+        {
+    recomendador: "Lara Gómez",
+    fotoRecomendador: "img/avatares/autor25.jpeg",
+    caratula: "img/portadas/libro25.jpeg",
+    titulo: "Calabobos",
+    autor: "Luis Mario",
+    editorial: "Reservoir Books",
+    resenya: "El verano es ese momento del año en el que uno regresa al pueblo y vuelve a ver a los familiares y amigos de toda la vida. Luis Mario también lo hace, pero pide que no idealicemos estos lugares, ya que “no son tan bucólicos como nos empeñamos que sean”. Eso no quita que no sirvan de inspiración para las novelas, incluida la suya, Calabobos. Se trata de un relato mitológico que invita al lector a recorrer Cantabria, sus gentes y seres fantásticos con un lenguaje oral que invita a viajar a cualquiera."
+  },
+      {
+    recomendador: "Alexis Racionero Ragué",
+    fotoRecomendador: "img/avatares/autor26.jpeg",
+    caratula: "img/portadas/libro26.png",
+    titulo: "Camino al cielo. Encuentros con ermitaños chinos",
+    autor: "Bill Porter",
+    editorial: "Tres Portales",
+    resenya: "Este es un libro para los que viajamos en busca de la sabiduría perenne, remota y antigua. Ensayo narrativo sobre la quimérica búsqueda de los últimos ermitaños taoístas perdidos en los montes Zhongnan. Ascetas de otro tiempo que armonizan con la naturaleza y la contemplan como algo sagrado. Calmar la mente y comprender que la nada es el cuerpo del Tao… Es preciso cultivar la tranquilidad y el desprendimiento. La vida es viaje."
+  },
+        {
+    recomendador: "Francesc Bombí Vilaseca",
+    fotoRecomendador: "img/avatares/autor27.jpeg",
+    caratula: "img/portadas/libro27.webp",
+    titulo: "Sau",
+    autor: "Ferran Garcia",
+    editorial: "Males Herbes",
+    resenya: "Esclavos guineanos trasplantados al corazón de Catalunya, un fantasma, dos cowboys crepusculares mientras se construye el pantano de Sau, pueblo inmerso en un cambio de época que lo arrasará todo, historias que, como afluentes, desembocan en un río principal donde todo se embalsa y las aguas se salen de madre. La novela más ambiciosa de Garcia, que ya había sorprendido los lectores con Guilleries."
+  },
+
+];
+
+const slider = document.getElementById('slider');
+const resena = document.getElementById('resena');
+
+libros.forEach((libro) => {
+  const slide = document.createElement('div');
+  slide.className = 'swiper-slide';
+  slide.innerHTML = `
+    <div class="recomanador">
+      <img class="foto" src="${libro.fotoRecomendador}" alt="Foto de ${libro.recomendador}" />
+      <div class="nombre">${libro.recomendador}</div>
+    </div>
+    <div class="portadaL">
+      <img class="caratula" src="${libro.caratula}" alt="Carátula de ${libro.titulo}" />
+    </div>
+    <div class="info">
+      <div class="titulo">${libro.titulo}</div>
+      <div class="autor">${libro.autor}</div>
+      <div class="editorial">${libro.editorial}</div>
+    </div>
+  `;
+  slide.addEventListener('click', () => {
+    resena.textContent = libro.resenya;
+  });
+  slider.appendChild(slide);
+});
+
+// resenya inicial
+resena.textContent = libros[0].resenya;
+
+// Inicializar Swiper
+new Swiper(".mySwiper", {
+  slidesPerView: 2,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 3
+    }
+  }
+});
